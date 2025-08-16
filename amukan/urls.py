251e76bcb7f0180bf6fbee36939ba13dddd04fd9
ethan <ethan.yanez@usm.cl>
2025-08-16 19:27:41 -0400

    👥 Beta Autenticación

--- amukan/urls.py
+++ amukan/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from apps.users.views import CreateUserView
+from apps.users.views import CreateUserView, CurrentUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
@@ -27,4 +27,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='get_token'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh'),
     path('api-auth/', include("rest_framework.urls")),
+    path("api/user/me/", CurrentUserView.as_view(), name="current_user"),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -3,9 +3,21 @@ from .models import CustomUser
 from rest_framework import generics
 from .serializers import UserSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 
 class CreateUserView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+class CurrentUserView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({
+            "username": request.user.username,
+            "email": request.user.email,
+        })
 
